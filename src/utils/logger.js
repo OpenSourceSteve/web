47b9029328df339a6a039e3f24ca7b1225e9c09b
Steve Sharp <stevenson.sharp@gmail.com>
2023-08-14 13:42:05 -0700

    Logger fix

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,11 +1,12 @@
-import { createPostJsonOptions, getEnv } from "./"
+import { createPostJsonOptions } from "./functions/createPostJsonOptions"
+import { getEnv } from "./functions/getEnv"
 import { LAMBDAS } from "../app/lambdas"
 
 export const logger = () => {
 
     const ENV = getEnv()
 
-    const loggerURL = LAMBDAS[ENV].loggerURL;
+    const { loggerURL } = LAMBDAS[ENV];
 
     const info = data => {
         console.log(data)
