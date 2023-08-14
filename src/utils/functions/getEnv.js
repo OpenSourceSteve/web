ab8bfec2be340f6a29d96f9448818df7e59b9884
Steve Sharp <stevenson.sharp@gmail.com>
2023-08-14 12:31:46 -0700

    Remove logging

--- src/utils/functions/getEnv.js
+++ src/utils/functions/getEnv.js
@@ -1,9 +1,11 @@
 export const getEnv = () => {
     const host = window.location.host
-    console.log(host)
+
     if (host === "localhost:3000") {
         return "dev"
     }
+
     const ENV = host.split(".")[0]
+
     return ENV === "www" ? "prod" : ENV
 }
