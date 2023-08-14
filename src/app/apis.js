e25318115d80e720c94164367177a44a72693394
Steve Sharp <stevenson.sharp@gmail.com>
2023-08-14 15:09:54 -0700

    Parameterize graphql APIs

---
+++ src/app/apis.js
@@ -0,0 +1,5 @@
+export const APIs = {
+    dev: "https://ho2ezw3tancixnygtz324wd3qe.appsync-api.us-west-1.amazonaws.com",
+    demo: "https://hg2rmqv7p5ad7cevcr5m7g44j4.appsync-api.us-west-1.amazonaws.com",
+    prod: ""
+}
