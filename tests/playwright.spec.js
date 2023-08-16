5f6d3e70902eeee95e98eca47521b3ef83044a86
Steve Sharp <stevenson.sharp@gmail.com>
2023-08-16 16:08:14 -0700

    Add Husky

---
+++ tests/playwright.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test');
+const { projects } = require('../playwright.config')
+
+test('webkit is commented', async () => {
+
+  let isCommented = true
+
+  projects.forEach(project => {
+    if (project.name === "webkit") {
+        isCommented = false
+    }
+  })
+
+  expect(isCommented).toBeTruthy()
+});
