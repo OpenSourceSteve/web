c570b92c532d8b5fa42bf8f00a26de3bc9c862f4
Steve Sharp <stevenson.sharp@gmail.com>
2023-08-22 11:12:59 -0700

    Revert playwright config change

--- tests/playwright.spec.js
+++ tests/playwright.spec.js
@@ -5,11 +5,11 @@ test('webkit is commented', async () => {
 
   let isCommented = true
 
-  // projects.forEach(project => {
-  //   if (project.name === "webkit") {
-  //       isCommented = false
-  //   }
-  // })
+  projects.forEach(project => {
+    if (project.name === "webkit") {
+        isCommented = false
+    }
+  })
 
   expect(isCommented).toBeTruthy()
 });
