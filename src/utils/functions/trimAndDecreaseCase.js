b4ddf2640ec0ce5fee00d92ab7a6d80412817a52
Steve Sharp <stevenson.sharp@gmail.com>
2023-09-05 09:15:11 -0700

    Redirect to demo

--- src/utils/functions/trimAndDecreaseCase.js
+++ src/utils/functions/trimAndDecreaseCase.js
@@ -1,3 +1,4 @@
+// TODO: make this a non-argument-mutating function, return updated clone
 export const trimAndDecreaseCase = (obj, fields) => {
     fields.forEach(field => {
       obj[field] = obj[field].trim().toLowerCase()
